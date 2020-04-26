11fe39b4acba235c7e266a370ba5b2bb6fe6af22
Vlasov Roman <spbroma@gmail.com>
2020-04-26 23:06:58 +0300

    upd

--- plot_and_push.sh
+++ plot_and_push.sh
@@ -4,16 +4,16 @@ page_path_1='../spbroma.github.io/covid'
 page_path_2='../spbroma.github.io/covid/days'
 page_path_3='../spbroma.github.io/covid/new'
 
-python plot_plotly.py
-python plot_plotly_days.py
-python plot_plotly_new.py
+python3 plot_plotly.py
+python3 plot_plotly_days.py
+python3 plot_plotly_new.py
 
-# cp covid.html $page_path_1/covid.html
-# cp covid_days.html $page_path_2/covid.html
-# cp covid_new.html $page_path_3/covid.html
+cp covid.html $page_path_1/covid.html
+cp covid_days.html $page_path_2/covid.html
+cp covid_new.html $page_path_3/covid.html
 
-# cd $page_path_1/..
+cd $page_path_1/..
 
-# git add . && git commit -m 'upd' && git push
+git add . && git commit -m 'upd' && git push
 
 # read -p 'Press any key to continue...' tmpvar
